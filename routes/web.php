37a04ef305a8a6f500151c7ebe8eb752464f27ed
Abdullah <abdullahrasool952@gmail.com>
2025-02-18 13:28:11 -0800

    code kharab

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ Route::middleware(['auth'])->group(function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
-    
+    ads' ajdfas
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
